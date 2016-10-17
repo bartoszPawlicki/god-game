e64464eb932f01369cdb7e323a7d2e5c5f9e4e63
Łukasz Zimoń <lukasz.zimon@outlook.com>
2016-10-17 18:42:23 +0200

    Should Work

---
+++ God Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 100;
+    private Rigidbody _rigidbody;
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, _rigidbody.velocity.y, moveVertical);
+        _rigidbody.velocity = movement;
+    }
+}
