50179732ebbc5d8e55bb0c406c51676a58d60721
Łukasz Zimoń <lukasz.zimon@outlook.com>
2016-10-19 15:09:36 +0200

    Basic camera tracks players

--- God Game/Assets/Scripts/CameraController.cs
+++ God Game/Assets/Scripts/CameraController.cs
@@ -3,17 +3,25 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject player1;
+    public GameObject player2;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+    Vector3 worldPosition;
     Vector3 offset;
 	// Use this for initialization
 	void Start ()
     {
-        offset = transform.position - player.transform.position;
+        offset = new Vector3(offsetX, offsetY, offsetZ);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = player.transform.position + offset;
+        worldPosition = Vector3.Lerp(player1.transform.position, player2.transform.position, 0.5f);
+        transform.position = worldPosition + offset;
+        transform.LookAt(worldPosition);
     }
 }
