38d05876b166c8f60e4ff673c88c66b8323eda2a
Adrian Jedlinski <ajedlinsk@gmail.com>
2016-11-07 21:41:47 +0100

    Portal

---
+++ God Game/Assets/UV_Scroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UV_Scroll : MonoBehaviour {
+    private Material mat;
+    private float scroll;
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scroll = (scroll - Time.deltaTime * 0.4f) % 1.0f;
+        mat.mainTextureOffset = new Vector2(scroll, 0);
+	}
+}
