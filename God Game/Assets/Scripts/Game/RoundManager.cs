328866b338b2a6c05755347bfacd000709ac721e
przemyslawgesieniec <przemyslaw.gesieniec@gmail.com>
2016-11-14 21:45:04 +0100

    prefab for player2 to prevent control problems

---
+++ God Game/Assets/Scripts/Game/RoundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundManager : MonoBehaviour {
+
+
+    private GameObject[] _players;
+
+    void Start ()
+    {
+        _players = GameObject.FindGameObjectsWithTag("Player");
+        short i = 0;
+        foreach (var player in _players)
+        {
+            player.transform.position = new Vector3(-5 + i, 1, 0);
+            i += 10;
+        }
+    }
+	
+	
+	void Update ()
+    {
+	
+	}
+}
