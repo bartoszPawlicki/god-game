5080eebb6043cb83efc7213c6ed8059218580e88
Adrian Jedlinski <ajedlinsk@gmail.com>
2016-11-21 17:18:59 +0100

    Added skill icons

---
+++ God Game/Assets/Scripts/SkillIcon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillIcon : MonoBehaviour {
+
+    public Image skillHighlight;
+    public Image skillSelectionOverlay;
+
+	void Start ()
+    {
+        SetSelected(false);
+	}
+
+    public void SetSelected(bool isSelected)
+    {
+        skillSelectionOverlay.enabled = isSelected;
+    }
+
+    public void SetCooldownPercent(float percent)
+    {
+        skillHighlight.fillAmount = percent / 100.0f;
+    }
+
+    /// <summary>
+    /// Accepts arguments from 0 to 1
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetCooldownValue(float value)
+    {
+        skillHighlight.fillAmount = value;
+    }
+}
