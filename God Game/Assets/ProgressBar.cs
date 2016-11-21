e8a0da5da835bbc142667acb1932c2640dea1155
Adrian Jedlinski <ajedlinsk@gmail.com>
2016-11-21 20:10:40 +0100

    Added HP bar for god

---
+++ God Game/Assets/ProgressBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+    public RectTransform progressBarTransform;
+
+	/// <summary>
+    /// Accepts values from 0 to 1
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetProgress(float value)
+    {
+        progressBarTransform.anchorMax = new Vector2(1 - value / 2, 1);
+        progressBarTransform.anchorMin = new Vector2(value / 2, 0);
+    }
+}
