156854130a47ceb0f05f551b0eca8d9a6ec327f8
Bartek <bartekp44@gmail.com>
2016-12-04 14:57:21 +0100

    Add code for visualisation wind direction (without physical effect yet)

---
+++ God Game/Assets/WindDirectionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindDirectionController : MonoBehaviour
+{
+    public float _windDirectionX { get; set; }
+    public float _windDirectionZ { get; set; }
+	// Use this for initialization
+	void Start ()
+    {
+        _globalWindController = GetComponentInParent<GlobalWindController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _windDirectionX = _globalWindController.AimHorizontal;
+        _windDirectionZ = _globalWindController.AimVertical;
+
+        Debug.Log("_windDirectionX: " + _windDirectionX);
+        Debug.Log("_windDirectionZ: " + _windDirectionZ);
+    }
+    
+    private GlobalWindController _globalWindController;
+}
