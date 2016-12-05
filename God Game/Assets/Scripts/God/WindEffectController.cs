fe5b84243286eb77ac9cc89630aa4435d5271db2
Adrian Jedlinski <ajedlinsk@gmail.com>
2016-12-05 13:18:16 +0100

    Added wind effect

---
+++ God Game/Assets/Scripts/God/WindEffectController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WindEffectController : MonoBehaviour {
+    public ParticleSystem particles;
+
+    public void SetEnabled(bool enabled)
+    {
+        ParticleSystem.EmissionModule em = particles.emission;
+        if (enabled)
+            em.rate = 50;
+        else
+            em.rate = 0;
+    }
+}
