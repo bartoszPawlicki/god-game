8d52a6339f5330da5b2f57df62ee2aaa2c59007f
Adrian Jedlinski <ajedlinsk@gmail.com>
2016-12-05 17:31:56 +0100

    Added lightning effect

---
+++ God Game/Assets/Scripts/God/LightningEffectController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LightningEffectController : MonoBehaviour {
+
+    public Texture2D[] lightningSprites;
+    private Material material;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+	void OnEnable()
+    {
+        StartCoroutine(RandomizerCoroutine());
+    }
+
+    private IEnumerator RandomizerCoroutine()
+    {
+        while(enabled)
+        {
+            yield return new WaitForSeconds(.25f);
+            transform.rotation = Quaternion.Euler(new Vector3(0, Random.value * 360));
+            material.mainTexture = lightningSprites[Mathf.RoundToInt(Random.value * (lightningSprites.Length -1))];
+        }
+    }
+}
