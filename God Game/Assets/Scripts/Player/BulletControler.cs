87e1139c4268a41e1ed34b580b732c924ee7ae96
przemyslawgesieniec <przemyslaw.gesieniec@gmail.com>
2016-12-20 00:01:46 +0100

    cooldown for bullet

--- God Game/Assets/Scripts/Player/BulletControler.cs
+++ God Game/Assets/Scripts/Player/BulletControler.cs
@@ -10,27 +10,37 @@ public class BulletControler : MonoBehaviour
     public float shotSpeed;
     public float shotStrength;
     public int damage;
+    public int SlinkShotCooldown;
 
     public int Direction { get; private set; }
+    public float SlinkShootValue
+    {
+        get
+        {
+            return _slinkShotCD.Loading;
+        }
+    }
     void Start ()
     {
         bulletPrefab = Resources.Load("bullet") as GameObject;
+        _slinkShotCD = new CooldownProvider(SlinkShotCooldown);
         _playerNumber = (int)char.GetNumericValue(transform.gameObject.name[transform.gameObject.name.Length - 1]);
     }
 	void Update ()
     {
-      //  if(Input.GetMouseButtonDown(0))
-        if (Input.GetAxis("Bullet_" + _playerNumber) == 1)
+        if (Input.GetAxis("Bullet_" + _playerNumber) == 1 && SlinkShootValue == 100)
         {
-            Debug.Log("shooot@");
+            _slinkShotCD.Use();
             GameObject newBullet = Instantiate(bulletPrefab) as GameObject;
             newBullet.transform.position = transform.position + gameObject.transform.forward;
             Rigidbody rb = newBullet.GetComponent<Rigidbody>();
             rb.velocity = gameObject.transform.forward * shotSpeed;
+            _slinkShotCD.Start();
         }
 	}
 
     private GameObject bulletPrefab;
+    private CooldownProvider _slinkShotCD;
     private int _playerNumber;
 
 }
