9336fc6a517857e2018e8e666fddd9303f964882
przemyslawgesieniec <przemyslaw.gesieniec@gmail.com>
2016-12-19 23:34:36 +0100

    slinkshot shooting without cooldown (Y button)

---
+++ God Game/Assets/Scripts/Player/BulletControler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Assets.Scripts.Utils;
+
+public class BulletControler : MonoBehaviour
+{
+
+    public float shotRange;
+    public float shotSpeed;
+    public float shotStrength;
+    public int damage;
+
+    public int Direction { get; private set; }
+    void Start ()
+    {
+        bulletPrefab = Resources.Load("bullet") as GameObject;
+        _playerNumber = (int)char.GetNumericValue(transform.gameObject.name[transform.gameObject.name.Length - 1]);
+    }
+	void Update ()
+    {
+      //  if(Input.GetMouseButtonDown(0))
+        if (Input.GetAxis("Bullet_" + _playerNumber) == 1)
+        {
+            Debug.Log("shooot@");
+            GameObject newBullet = Instantiate(bulletPrefab) as GameObject;
+            newBullet.transform.position = transform.position + gameObject.transform.forward;
+            Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+            rb.velocity = gameObject.transform.forward * shotSpeed;
+        }
+	}
+
+    private GameObject bulletPrefab;
+    private int _playerNumber;
+
+}
