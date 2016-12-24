dbc8f3e863c8e5f94c4d54e794c58274356ae194
Łukasz Zimoń <lukasz.zimon@outlook.com>
2016-12-24 13:07:55 +0100

    Add ground god lightning

---
+++ God Game/Assets/Scripts/God/GroundLightningEffectController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.God
+{
+    public class GroundLightningEffectController : MonoBehaviour
+    {
+        public Texture2D[] lightningSprites;
+        private Material material;
+
+        void Start()
+        {
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        void OnEnable()
+        {
+            StartCoroutine(RandomizerCoroutine());
+        }
+
+        private IEnumerator RandomizerCoroutine()
+        {
+            while (enabled)
+            {
+                yield return new WaitForSeconds(.25f);
+                transform.localEulerAngles = new Vector3(Random.value * 360, 90, 90);
+                material.mainTexture = lightningSprites[Mathf.RoundToInt(Random.value * (lightningSprites.Length - 1))];
+            }
+        }
+    }
+}
