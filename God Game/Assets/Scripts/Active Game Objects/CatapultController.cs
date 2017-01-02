7740f9fbdd92a4ab92945033af9db29e6e5edc87
Bartosz <bartekp44@gmail.com>
2017-01-02 15:14:23 +0100

    Add model and rotation to catapults

--- God Game/Assets/Scripts/Active Game Objects/CatapultController.cs
+++ God Game/Assets/Scripts/Active Game Objects/CatapultController.cs
@@ -8,6 +8,13 @@ public class CatapultController : MonoBehaviour
     public float CatapultVerticalStrength;
     public float CatapultHorizontalStrength;
     public Vector3 CatapultDirection;
+    public float PlankRotationInterval;
+    public int TotalRotationIntervals;
+
+    void Start()
+    {
+        _catapultPlank = transform.FindChild("catapult_plank").gameObject;
+    }
 
     void OnTriggerEnter(Collider collider)
     {
@@ -18,6 +25,26 @@ public class CatapultController : MonoBehaviour
 
             collider.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3 (0, CatapultDirection.y, 0).normalized * CatapultVerticalStrength);
             collider.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(CatapultDirection.x, 0, CatapultDirection.z).normalized * CatapultHorizontalStrength);
+
+            StartCoroutine(RotatePlank());
         }
     }
+
+    IEnumerator RotatePlank()
+    {
+        for (int i=0; i< TotalRotationIntervals; i++)
+        {
+            _catapultPlank.transform.Rotate(new Vector3(80/ TotalRotationIntervals, 0, 0));
+            yield return new WaitForSeconds(PlankRotationInterval/5);
+        }
+
+        for (int i = 0; i < TotalRotationIntervals; i++)
+        {
+            _catapultPlank.transform.Rotate(new Vector3(-80 / TotalRotationIntervals, 0, 0));
+            yield return new WaitForSeconds(PlankRotationInterval/2);
+        }
+
+    }
+
+    private GameObject _catapultPlank;
 }
