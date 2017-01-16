bf8269aab0577fca264b55d10d5655b3e1f7dc76
Adrian Jedlinski <ajedlinsk@gmail.com>
2017-01-16 21:16:30 +0100

    Temporary character models with animations

---
+++ God Game/Assets/AnimatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour {
+    public int speed;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animator>().SetInteger("Speed", speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
