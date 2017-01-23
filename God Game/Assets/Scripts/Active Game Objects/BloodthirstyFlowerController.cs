8e565c74ed4777722a2fb29f94e84a3fbb573b84
Łukasz Zimoń <lukasz.zimon@outlook.com>
2017-01-23 17:30:28 +0100

    Flower collider

--- God Game/Assets/Scripts/Active Game Objects/BloodthirstyFlowerController.cs
+++ God Game/Assets/Scripts/Active Game Objects/BloodthirstyFlowerController.cs
@@ -26,6 +26,7 @@ public class BloodthirstyFlowerController : MonoBehaviour
                 {
                     _hp = 0;
                     enabled = false;
+                    gameObject.SetActive(false);
                 }
             }
         }
--- God Game/Assets/Scripts/Active Game Objects/BloodthirstyFlowerControllerTraining.cs
+++ God Game/Assets/Scripts/Active Game Objects/BloodthirstyFlowerControllerTraining.cs
@@ -26,6 +26,7 @@ public class BloodthirstyFlowerControllerTraining : MonoBehaviour
                 {
                     _hp = 0;
                     enabled = false;
+                    gameObject.SetActive(false);
                 }
             }
         }
