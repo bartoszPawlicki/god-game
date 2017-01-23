ec026c3f3ee9de95d74880ec000f9863fd10732a
Łukasz Zimoń <lukasz.zimon@outlook.com>
2017-01-24 00:52:44 +0100

    Player respawn on second training island when the reache it.

---
+++ God Game/Assets/TrainingIslandController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingIslandController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        _gameController = GameObject.Find("Game").GetComponent<GameController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            _gameController.StartPosition = new Vector3(-3,1,-31);
+    }
+
+    private GameController _gameController;
+}
